1341601bb8b2fff13b477e57a4c5a409ba1f645a
sjaakp <sjaak@sjaakpriester.nl>
2015-09-22 14:54:06 +0200

    First upload

---
+++ Telex.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * MIT licence
+ * Version 1.0.1
+ * Sjaak Priester, Amsterdam 22-09-2015 ... .
+ *
+ * News scroller widget for Yii 2.0 framework
+ */
+
+namespace sjaakp\telex;
+
+use yii\base\Widget;
+use yii\base\InvalidConfigException;
+use yii\helpers\Html;
+use yii\helpers\Json;
+
+
+class Telex extends Widget {
+    /**
+     * @var array
+     * Initial messages
+     */
+    public $messages = [];
+
+    /**
+     * @var array
+     * Client options for the Telex jQuery widget.
+     * @link https://github.com/sjaakp/telex
+     */
+    public $options = [];
+
+    /**
+     * @var array
+     * HTML options of the dateline container.
+     * Use this if you want to explicitly set the ID.
+     */
+    public $htmlOptions = [];
+
+    public function init()  {
+        if (isset($this->htmlOptions['id'])) {
+            $this->setId($this->htmlOptions['id']);
+        }
+        else $this->htmlOptions['id'] = $this->getId();
+    }
+
+
+    public function run()   {
+        $view = $this->getView();
+
+        $asset = new TelexAsset();
+        $asset->register($view);
+
+        $jOpts = count($this->options) ? Json::encode($this->options) : '{}';
+
+        $id = $this->getId();
+
+        $js = "var {$id}=$('#{$id}').telex($jOpts);";
+
+        $view->registerJs($js);
+
+        $msgDivs = array_map(function($v) {
+            if (! isset($v['text'])) return '';
+            $opts = [];
+            if (isset($v['class'])) $opts['class'] = $v['class'];
+            return Html::tag('div', $v['text'], $opts);
+        }, $this->messages);
+
+        echo Html::tag('div', implode("\n", $msgDivs), $this->htmlOptions);
+    }
+}
---
+++ TelexAsset.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * MIT licence
+ * Version 1.0.0
+ * Sjaak Priester, Amsterdam 22-09-2015.
+ *
+ * News scroller widget for Yii 2.0 framework
+ */
+
+namespace sjaakp\telex;
+
+use yii\web\AssetBundle;
+
+class TelexAsset extends AssetBundle {
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+//    public $sourcePath = '@bower/telex/dist';
+    public $sourcePath = '@webroot/github/telex/dist';
+
+    public $css = [
+        'telex.css'
+    ];
+
+    public function init()    {
+        parent::init();
+
+        $this->js[] = YII_DEBUG ? 'telex.js' : 'telex.min.js';
+    }
+}
